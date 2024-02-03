f7c78db18882d5cc607ec8d8d6e1052f768741f0
unknown <mhsfn00@gmail.com>
2024-02-02 21:41:14 -0300

    changed the subject-date and title size for secondary posts

--- src/components/PostContainerGrid.vue
+++ src/components/PostContainerGrid.vue
@@ -17,10 +17,10 @@ onMounted(async () => {
                 <div class="main-post-card">
                     <router-link class="link-nostyling image-container" :to="{path:`post/${postsStore.mainPost.id}`}">
                             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Athletics_%2836099368890%29.jpg/1280px-Athletics_%2836099368890%29.jpg" class="post-image" />
-                            <div class="shadow-overlay" />
+                            <div class="shadow-overlay"></div>
                             <div class="overlay-text">
-                                <p class="subject-date">{{ "Track and Field | " + postsStore.mainPost.date }}</p>
-                                <p class="title">{{ postsStore.mainPost.title }}</p>
+                                <p class="main-subject-date">{{ "Track and Field | " + postsStore.mainPost.date }}</p>
+                                <p class="main-title">{{ postsStore.mainPost.title }}</p>
                             </div>
                     </router-link>
                 </div>
@@ -28,10 +28,10 @@ onMounted(async () => {
                     <div v-for="(post, index) in postsStore.secondaryPosts" :key="index" class="secondary-post-card">
                         <router-link class="link-nostyling image-container" :to="{path:`post/${post.id}`}">
                             <img src="//live.staticflickr.com/5021/5618703271_c8231cfe1c_h.jpg" class="post-image" />
-                            <div class="shadow-overlay" />
+                            <div class="shadow-overlay"></div>
                             <div class="overlay-text">
-                                <p class="subject-date">{{ "Track and Field | " + post.date }}</p>
-                                <p class="title">{{ post.title }}</p>
+                                <p class="secondary-subject-date">{{ "Track and Field | " + post.date }}</p>
+                                <p class="secondary-title">{{ post.title }}</p>
                             </div>
                         </router-link>
                     </div>
@@ -41,10 +41,10 @@ onMounted(async () => {
                 <div v-for="(post, index) in postsStore.extraPosts" :key="index" class="extra-post-card">
                     <router-link class="link-nostyling image-container" :to="{path:`post/${post.id}`}">
                         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/For_athletics.jpg/800px-For_athletics.jpg" class="post-image" />
-                        <div class="shadow-overlay" />
+                        <div class="shadow-overlay"></div>
                         <div class="overlay-text">
-                            <p class="subject-date">{{ "Track and Field | " + post.date }}</p>
-                            <p class="title">{{ post.title }}</p>
+                            <p class="secondary-subject-date">{{ "Track and Field | " + post.date }}</p>
+                            <p class="secondary-title">{{ post.title }}</p>
                         </div>
                     </router-link>
                 </div>
@@ -63,18 +63,27 @@ onMounted(async () => {
     border-radius: 5px;
 }
 
-.subject-date {
+.main-subject-date {
     font-size: 20px;
 }
 
-.title {
+.main-title {
     font-size: 30px;
+}
+
+.secondary-title {
+    font-size: 25px;
+
+}
+
+.secondary-subject-date {
+    font-size: 15px;
 }
 
 .overlay-text {
     position: absolute;
-    left: 16px;
-    bottom: 30px;
+    left: 2%;
+    bottom: 5%;
     font-weight: bold;
 }
 
