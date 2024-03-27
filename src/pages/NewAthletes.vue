2519f0b5b430e14e5c196f2093f70dae584bbe3e
unknown <mhsfn00@gmail.com>
2024-03-27 20:30:25 -0300

    minor

--- src/pages/NewAthletes.vue
+++ src/pages/NewAthletes.vue
@@ -73,7 +73,7 @@ async function confirmAthletes() {
     //     alert("Todos os atletas foram inseridos");
     // }
 
-    // showAlertConfirm.value = false;
+    showAlertConfirm.value = false;
 }
 </script>
 
