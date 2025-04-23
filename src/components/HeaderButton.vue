0063b709870f09178831ad385d43b5c02cffd5c3
mhsfn00 <mhsfn00@gmail.com>
2025-04-23 16:51:16 -0300

    started mobile header development

---
+++ src/components/HeaderButton.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps ({
+    imageSource: { type: String, default: '' }
+})
+</script>
+
+<template>
+  <div class="button-container">
+      <img class="icon" :src="imageSource" />
+  </div>
+</template>
+
+<style scoped>
+.button-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 5vh;
+    padding: 0 10px;
+}
+
+.icon {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import HeaderButton from './HeaderButton.vue'
+</script>
+
+<template>
+    <div class="header-container">
+        <HeaderButton :imageSource="'/public/images/logo.png'"/>
+        <HeaderButton :imageSource="'/public/images/menu.png'"/>
+    </div>
+</template>
+
+<style scoped>
+.header-container {
+  background-color: var(--primary-green);
+  color: var(--primary-black);
+  position: absolute;
+  width: 100%;
+  height: 7vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
