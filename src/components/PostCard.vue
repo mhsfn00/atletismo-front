141a80cef6da212383e059088acf9bfa6e8a4350
mhsfn00 <mhsfn00@gmail.com>
2025-05-05 17:18:22 -0300

    started the development of the main page (mobile)

---
+++ src/components/PostCard.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+defineProps({
+    post: { type: Object, default: () => ({}) }
+})
+</script>
+
+<template>
+    <div :class="['card-container', { main: post.mainPost }]">
+        <div class="image-container">
+            <img class="image" :src="post.imageAddress">
+        </div>
+        <div class="text-container">
+            <p>{{ post.title }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card-container {
+    display: flex;
+    padding: 1vh 0;
+    border-bottom: 2px solid var(--primary-black);
+}
+.main {
+    flex-direction: column;
+}
+
+.image {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+}
+
+.text-container {
+    background-color: lightblue;
+}
+
+.image-container {
+    width: 100%;
+    height: 100px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,37 @@
 <script setup lang="ts">
-
+import PostCard from '/src/components/PostCard.vue';
 </script>
 
 <template>
     <div class="page-container">
-        <h1>HOMEPAGE</h1>
+        <PostCard 
+            :post="{
+                'mainPost': true,
+                'imageAddress': '/public/images/defaultPostImage.png',
+                'title': 'Titulo post 1'
+            }" 
+        />
+        <PostCard 
+            :post="{
+                'mainPost': false,
+                'imageAddress': '/public/images/defaultPostImage.png',
+                'title': 'Titulo post 2'
+            }" 
+        />
+        <PostCard 
+            :post="{
+                'mainPost': false,
+                'imageAddress': '/public/images/defaultPostImage.png',
+                'title': 'Titulo post 3'
+            }" 
+        />
+        <PostCard 
+            :post="{
+                'mainPost': false,
+                'imageAddress': '/public/images/defaultPostImage.png',
+                'title': 'Titulo post 4'
+            }" 
+        />
     </div>
 </template>
 
